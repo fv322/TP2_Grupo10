2a2016e54d19ce1341061ff9c564f0b849bd6a82
fv322 <38471258@fi.unju.edu.ar>
2022-04-12 22:46:10 -0300

    tp2

---
+++ TP2_Grupo10/src/main/java/ar/edu/unju/fi/tp2/Tp2Grupo10Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Grupo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Grupo10Application.class, args);
+	}
+
+}
---
+++ TP2_Grupo10/src/test/java/ar/edu/unju/fi/tp2/Tp2Grupo10ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2Grupo10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
