23b7053f6d97d2450c9c53e6f5dd39bf20031d93
fv322 <38471258@fi.unju.edu.ar>
2022-04-13 10:50:56 -0300

    Declaracion del calculo de factorial

--- TP2_Grupo10/src/main/java/ar/edu/unju/fi/factorial/Principal.java
+++ TP2_Grupo10/src/main/java/ar/edu/unju/fi/factorial/Principal.java
@@ -1,9 +1,27 @@
 package ar.edu.unju.fi.factorial;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		System.out.println("Ingrese numero: ");
+		Scanner entrada = new Scanner(System.in);
+		int numero = entrada.nextInt();
+
+		int aux = 1;
+		if ((numero >= 0) && (numero <= 10)) {
+			for (int i = 1; i <= numero; i++) {
+				aux *= i;
+			}
+
+			System.out.println("El factorial de " + numero + " es " + aux);
+		} else {
+			System.out.println(" El numero ingresado esta fuera del rango permitido");
+
+		}
 
 	}
 
